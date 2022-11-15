3a60fff36b2217f86da093fc9a7b5c1db931ace0
GSilvaO <gsoliveira.x@gmail.com>
2022-11-15 14:47:41 -0300

    create AlunosRepositoryEmMemoria

--- src/main/java/br/com/gsilvao/escola/infra/aluno/AlunosRepositoryEmMemoria.java
+++ src/main/java/br/com/gsilvao/escola/infra/aluno/AlunosRepositoryEmMemoria.java
@@ -1,29 +1,33 @@
 package br.com.gsilvao.escola.infra.aluno;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.gsilvao.escola.dominio.aluno.Aluno;
+import br.com.gsilvao.escola.dominio.aluno.AlunoNaoEncontrado;
 import br.com.gsilvao.escola.dominio.aluno.AlunosRepository;
 import br.com.gsilvao.escola.dominio.aluno.CPF;
 
 public class AlunosRepositoryEmMemoria implements AlunosRepository {
+	
+	private List<Aluno> matriculados = new ArrayList<>();
 
 	@Override
 	public void matricular(Aluno aluno) {
-		// TODO Auto-generated method stub
-
+		this.matriculados.add(aluno);
 	}
 
 	@Override
 	public Aluno buscarPorCPF(CPF cpf) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.matriculados.stream()
+			.filter(a -> a.getCpf().equals(cpf.getNumero()))
+			.findFirst()
+			.orElseThrow(() -> new AlunoNaoEncontrado(cpf));
 	}
 
 	@Override
 	public List<Aluno> listarTodosAlunosMatriculados() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.matriculados;
 	}
 
 }
